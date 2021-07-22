a5699a1d320801de26c2588ed343ca2fb21b6b8a
Frank Schüler <SchuelerFrank@gmx.de>
2021-07-22 11:26:32 +0200

    initial

---
+++ bundle-browser.js
@@ -0,0 +1,11 @@
+import SETTING from "./src/Setting";
+import Modal from "./src/Modal";
+import Content from "./src/Content";
+
+window.defaultjs = window.defaultjs || {};
+window.defaultjs.html = window.defaultjs.html || {};
+window.defaultjs.html.modal = defaultjs.html.modal || {
+	Modal,
+	Content,
+	SETTING
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+import SETTING from "./src/Setting";
+import Modal from "./src/Modal";
+import Content from "./src/Content";
+
+
+export {
+    SETTING,
+    Modal,
+    Content
+}
