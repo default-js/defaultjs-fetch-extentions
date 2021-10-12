5b0ab59ee258234cb978cfb4d5264ccf8b99271a
Frank Schüler <Frank.Schueler@fiduciagad.de>
2021-10-12 10:52:58 +0200

    bugfix release 1.2.3

--- src/XmlAsJson.js
+++ src/XmlAsJson.js
@@ -1,13 +1,15 @@
 import xmlToJson from "@default-js/defaultjs-common-utils/src/converter/XmlToJson";
 
 const CONTENTTYPE = "Content-Type";
+const CONTENTTYPE_LOWERCASE = CONTENTTYPE.toLowerCase();
 const ALLOWED_CONTENTTYPE_REGEX = /.*xml.*/ig
 const ALLOWED_FILE_EXTENTION_REGEX = /.+\.(xml)/ig
 
 const orgJson = Response.prototype.json;
 Response.prototype.json = async function(){
 
-    const contentType = this.headers.get[CONTENTTYPE];
+    const headers = this.headers;
+    const contentType = headers.get(CONTENTTYPE) || headers.get(CONTENTTYPE_LOWERCASE);
 
     let convertToJson = false;
     if(contentType)
